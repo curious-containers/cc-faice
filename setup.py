7090ef5dc731aebf343953ddd6dd28cdc2ad8bda
Christoph Jansen <christoph@gnork.org>
2018-01-07 19:50:15 +0100

    init

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+description = 'FAICE is part of the Curious Containers project and enables researchers to perform and distribute' \
+              'reproducible data-driven experiments.'
+
+setup(
+    name='faice',
+    version='2.0.0',
+    summary=description,
+    description=description,
+    author='Christoph Jansen',
+    author_email='Christoph.Jansen@htw-berlin.de',
+    url='https://github.com/curious-containers/cc-faice',
+    packages=[
+        'faice'
+    ],
+    entry_points={
+        'console_scripts': ['faice=faice.main:main']
+    },
+    license='AGPL-3.0',
+    platforms=['any'],
+    install_requires=[
+        'cc-core'
+    ]
+)

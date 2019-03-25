f55093862c7d61d0a5dcf9d0b57691292a50a3bb
Bluemi <bruno.schilling@protonmail.ch>
2019-03-25 17:18:20 +0100

    worked on redagent 2

--- cc_faice/agent/red2/main.py
+++ cc_faice/agent/red2/main.py
@@ -1,10 +1,29 @@
-from argparse import ArgumentParser
+"""
+                Host            Container
+blue_file       <tempfile>      /tmp/red/exec/blue_file.yml
+blue_agent      <import...>     /tmp/red/exec/blue_agent.py
+working dir     -               /tmp/red/work/
+outputs         ./outputs       /tmp/red/outputs/
+inputs          -               /tmp/red/inputs/
+"""
+import tempfile
+import json
 
+from argparse import ArgumentParser
+from enum import Enum
+from uuid import uuid4
+
+from cc_core.commons.exceptions import print_exception, exception_format
 from cc_core.commons.files import load_and_read
 from cc_core.commons.red import red_validation
 from cc_core.commons.red_to_blue import convert_red_to_blue
 
 DESCRIPTION = 'Run an experiment as described in a REDFILE with ccagent red in a container.'
+
+PYTHON_INTERPRETER = 'python3'
+BLUE_FILE_CONTAINER_PATH = '/tmp/red/exec/blue_file.json'
+BLUE_AGENT_CONTAINER_PATH = '/tmp/red/exec/blue_agent.py'
+CONTAINER_OUTPUT_DIRECTORY = '/tmp/red/outputs/'
 
 
 def attach_args(parser):
@@ -61,22 +80,27 @@ def _get_commandline_args():
     return parser.parse_args()
 
 
+class OutputMode(Enum):
+    Connectors = 0
+    Directory = 1
+
+
 def main():
     args = _get_commandline_args()
     run(**args.__dict__,
-        ignore_outputs=not args.outputs)
+        output_mode=OutputMode.Connectors if args.outputs else OutputMode.Directory)
 
 
 def run(red_file,
         # variables,
         # format,
         # disable_pull,
-        # leave_container,
+        leave_container,
         # preserve_environment,
         # non_interactive,
         # prefix,
         # insecure,
-        ignore_outputs,
+        output_mode,
         **_
         ):
     """
@@ -84,12 +108,14 @@ def run(red_file,
     :param red_file: The path or URL to the RED File to execute
     # :param variables: A path or URL to an variables file
     # :param disable_pull:
-    # :param leave_container:
+    :param leave_container:
     # :param preserve_environment:
     # :param non_interactive:
     # :param prefix:
     # :param insecure:
-    :param ignore_outputs:
+    :param output_mode: Either Connectors or Directory. If Directory Connectors, the blue agent will try to execute
+    the output connectors, if Directory faice will mount an outputs directory and the blue agent will move the output
+    files into this directory.
     """
 
     result = {
@@ -98,8 +124,102 @@ def run(red_file,
         'state': 'succeeded'
     }
 
-    red_data = load_and_read(red_file, 'REDFILE')
-    red_validation(red_data, ignore_outputs, container_requirement=True)
+    try:
+        red_data = load_and_read(red_file, 'REDFILE')
+        red_validation(red_data, output_mode == OutputMode.Directory, container_requirement=True)
 
-    blue_data = convert_red_to_blue(red_data)
-    print(blue_data)
+        blue_batches = convert_red_to_blue(red_data)
+
+        blue_agent_host_path = get_blue_agent_host_path()
+
+        # docker settings
+        docker_image = red_data['container']['settings']['image']['url']
+
+        host_outputs_dir = None  # TODO
+        if len(blue_batches) == 1:
+            host_outputs_dir = 'outputs'
+        else:
+            host_outputs_dir = 'outputs_{batch_index}'
+
+        for batch_index, blue_batch in enumerate(blue_batches):
+            run_blue_batch(blue_batch,
+                           docker_image,
+                           blue_agent_host_path,
+                           host_outputs_dir,
+                           output_mode,
+                           leave_container,
+                           batch_index)
+    except Exception as e:
+        print_exception(e)
+        result['debugInfo'] = exception_format()
+        result['state'] = 'failed'
+
+
+def run_blue_batch(blue_batch,
+                   image,
+                   blue_agent_host_path,
+                   host_outputs_dir,
+                   output_mode,
+                   leave_container,
+                   batch_index,
+                   ram):
+    """
+    Executes an blue agent inside a docker container that takes the given blue batch as argument.
+    :param blue_batch: The blue batch to execute
+    :param image: The docker image url to use. This docker image should be already present on the host machine
+    :param blue_agent_host_path: The path to the blue agent to execute
+    :param host_outputs_dir: The outputs directory of the host.
+    :param output_mode: If output mode == Connectors the blue agent will be started with '--outputs' flag
+    Otherwise this function will mount the CONTAINER_OUTPUT_DIRECTORY
+    :param leave_container: If True, the started container will not be stopped after execution.
+    :param batch_index: The index of the current batch
+    :param ram: TODO
+    :return: A container result as dictionary
+    """
+    container_name = str(uuid4())
+    command = _create_blue_agent_command()
+
+    blue_file = _create_json_file(blue_batch)
+
+    ro_mappings = [[blue_file.name, BLUE_FILE_CONTAINER_PATH],
+                   [blue_agent_host_path, BLUE_AGENT_CONTAINER_PATH]]
+
+    rw_mappings = []
+    if output_mode == OutputMode.Directory:
+        rw_mappings.append([host_outputs_dir.format(batch_index=batch_index), CONTAINER_OUTPUT_DIRECTORY])
+
+    working_directory = blue_batch['workDir']
+
+    # EXECUTE
+
+    blue_file.close()
+
+
+def _create_json_file(data):
+    """
+    Creates a temporary file that contains the given data in json format.
+    :param data: The data to write to the temporary file
+    :return: A NamedTemporaryFile
+    """
+    f = tempfile.NamedTemporaryFile()
+    json.dump(data, f)
+    f.seek(0)
+    f.flush()
+    return f
+
+
+def _create_blue_agent_command():
+    """
+    Defines the command to execute inside the docker container to execute the blue agent.
+    :return: A list of strings to execute inside the docker container.
+    """
+    return [PYTHON_INTERPRETER, BLUE_AGENT_CONTAINER_PATH, BLUE_FILE_CONTAINER_PATH]
+
+
+def get_blue_agent_host_path():
+    """
+    Returns the path of the blue agent in the host machine to mount into the docker container.
+    :return: The path to the blue agent
+    """
+    import cc_core.agent.blue.main as blue_main
+    return blue_main.__file__

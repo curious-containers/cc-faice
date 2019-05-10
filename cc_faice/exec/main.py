a4964f859c1bdc3b819ba7181b3e18b9ec21e367
Bluemi <bruno.schilling@protonmail.ch>
2019-05-10 15:35:14 +0200

    prettified faice exec error messages

--- cc_faice/exec/main.py
+++ cc_faice/exec/main.py
@@ -3,10 +3,11 @@ from copy import deepcopy
 
 import requests
 
-from cc_core.commons.files import load_and_read, wrapped_print, dump_print
+from cc_core.commons.exceptions import print_exception, exception_format
+from cc_core.commons.files import load_and_read, dump_print
 from cc_core.commons.red import red_validation
 from cc_core.commons.engines import engine_validation
-from cc_core.commons.templates import normalize_keys
+from cc_core.commons.templates import normalize_keys, get_secret_values
 
 from cc_faice.agent.red.main import run as run_faice_agent_red, OutputMode
 from cc_faice.commons.templates import complete_red_templates
@@ -18,6 +19,10 @@ def attach_args(parser):
     parser.add_argument(
         'red_file', action='store', type=str, metavar='REDFILE',
         help='REDFILE (json or yaml) containing an experiment description as local PATH or http URL.'
+    )
+    parser.add_argument(
+        '-d', '--debug', action='store_true',
+        help='Write debug info, including detailed exceptions, to stdout.'
     )
     parser.add_argument(
         '--non-interactive', action='store_true',
@@ -42,66 +47,80 @@ def main():
     parser = ArgumentParser(description=DESCRIPTION)
     attach_args(parser)
     args = parser.parse_args()
-    return run(**args.__dict__)
+    result = run(**args.__dict__, fmt=args.format)
 
+    if args.debug:
+        dump_print(result, args.format)
 
-def run(red_file, non_interactive, format, insecure, keyring_service):
-    red_data = load_and_read(red_file, 'REDFILE')
-    red_validation(red_data, False)
-    # TODO: nicer errors (e.g. if execution key is not available)
-    engine_validation(red_data, 'execution', ['ccfaice', 'ccagency'], 'faice exec')
-
-    # exec via CC-FAICE
-    # equivalent to `faice agent red --debug --outputs`
-    if red_data['execution']['engine'] == 'ccfaice':
-        result = run_faice_agent_red(red_file=red_file,
-                                     disable_pull=False,
-                                     leave_container=False,
-                                     preserve_environment=[],
-                                     non_interactive=False,
-                                     insecure=insecure,
-                                     output_mode=OutputMode.Connectors,
-                                     keyring_service='red')
-        dump_print(result, 'yaml')
-
-        if result['state'] == 'succeeded':
-            return 0
-
+    if result['state'] != 'succeeded':
         return 1
-
-    complete_red_templates(red_data, keyring_service, non_interactive)
-
-    red_data_normalized = deepcopy(red_data)
-    normalize_keys(red_data_normalized)
-
-    if 'access' not in red_data_normalized['execution']['settings']:
-        wrapped_print([
-            'ERROR: cannot send RED data to CC-Agency if access settings are not defined.'
-        ], error=True)
-        return 1
-
-    if 'auth' not in red_data_normalized['execution']['settings']['access']:
-        wrapped_print([
-            'ERROR: cannot send RED data to CC-Agency if auth is not defined in access settings.'
-        ], error=True)
-        return 1
-
-    access = red_data_normalized['execution']['settings']['access']
-
-    r = requests.post(
-        '{}/red'.format(access['url'].strip('/')),
-        auth=(
-            access['auth']['username'],
-            access['auth']['password']
-        ),
-        json=red_data
-    )
-    try:
-        r.raise_for_status()
-    except:
-        wrapped_print(r.text.split('\n'), error=True)
-        return 1
-
-    dump_print(r.json(), format)
 
     return 0
+
+
+def run(red_file, non_interactive, fmt, insecure, keyring_service, **_):
+    secret_values = None
+    result = {
+        'state': 'succeeded',
+        'debugInfo': None
+    }
+    try:
+        red_data = load_and_read(red_file, 'REDFILE')
+        red_validation(red_data, False)
+        # TODO: nicer errors (e.g. if execution key is not available)
+        engine_validation(red_data, 'execution', ['ccfaice', 'ccagency'], 'faice exec')
+
+        secret_values = get_secret_values(red_data)
+
+        # exec via CC-FAICE
+        # equivalent to `faice agent red --debug --outputs`
+        if 'execution' not in red_data:
+            raise KeyError('The key "execution" is needed in red file for usage with faice exec.')
+        if red_data['execution']['engine'] == 'ccfaice':
+            result = run_faice_agent_red(red_file=red_file,
+                                         disable_pull=False,
+                                         leave_container=False,
+                                         preserve_environment=[],
+                                         non_interactive=False,
+                                         insecure=insecure,
+                                         output_mode=OutputMode.Connectors,
+                                         keyring_service='red')
+            dump_print(result, 'yaml')
+
+            return result
+
+        complete_red_templates(red_data, keyring_service, non_interactive)
+
+        red_data_normalized = deepcopy(red_data)
+        normalize_keys(red_data_normalized)
+
+        if 'access' not in red_data_normalized['execution']['settings']:
+            result['debugInfo'] = ['ERROR: cannot send RED data to CC-Agency if access settings are not defined.']
+            result['state'] = 'failed'
+            return result
+
+        if 'auth' not in red_data_normalized['execution']['settings']['access']:
+            result['debugInfo'] = ['ERROR: cannot send RED data to CC-Agency if auth is not defined in access '
+                                   'settings.']
+            result['state'] = 'failed'
+            return result
+
+        access = red_data_normalized['execution']['settings']['access']
+
+        r = requests.post(
+            '{}/red'.format(access['url'].strip('/')),
+            auth=(
+                access['auth']['username'],
+                access['auth']['password']
+            ),
+            json=red_data
+        )
+        r.raise_for_status()
+
+        dump_print(r.json(), fmt)
+    except Exception as e:
+        print_exception(e, secret_values)
+        result['debugInfo'] = exception_format(secret_values)
+        result['state'] = 'failed'
+
+    return result

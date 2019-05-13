f12d4a7ff4c7da89fe6d1e42e195bc94f99c4d0a
Bluemi <bruno.schilling@protonmail.ch>
2019-05-13 13:40:02 +0200

    improved error messages of faice convert batches

--- cc_faice/convert/batches/main.py
+++ cc_faice/convert/batches/main.py
@@ -1,8 +1,9 @@
 from argparse import ArgumentParser
 
-from cc_core.commons.files import load_and_read, file_extension, wrapped_print, dump
+from cc_core.commons.exceptions import print_exception, exception_format
+from cc_core.commons.files import load_and_read, file_extension, wrapped_print, dump, dump_print
 from cc_core.commons.red import red_validation, convert_batch_experiment
-
+from cc_core.commons.templates import get_secret_values
 
 DESCRIPTION = 'Convert batches from a single REDFILE into separate files containing only one batch each.'
 
@@ -20,30 +21,55 @@ def attach_args(parser):
         '--prefix', action='store', type=str, metavar='PREFIX', default='faice_',
         help='PREFIX for files dumped to storage, default is "faice_".'
     )
+    parser.add_argument(
+        '-d', '--debug', action='store_true',
+        help='Write debug info, including detailed exceptions, to stdout.'
+    )
 
 
 def main():
     parser = ArgumentParser(description=DESCRIPTION)
     attach_args(parser)
     args = parser.parse_args()
-    return run(**args.__dict__)
 
+    result = run(**args.__dict__, fmt=args.format)
 
-def run(red_file, format, prefix):
-    ext = file_extension(format)
+    if args.debug:
+        dump_print(result, args.format)
 
-    red_data = load_and_read(red_file, 'REDFILE')
-    red_validation(red_data, False)
-
-    if 'batches' not in red_data:
-        wrapped_print([
-            'ERROR: REDFILE does not contain batches.'
-        ], error=True)
+    if result['state'] != 'succeeded':
         return 1
 
-    for batch in range(len(red_data['batches'])):
-        batch_data = convert_batch_experiment(red_data, batch)
-        dumped_batch_file = '{}batch_{}.{}'.format(prefix, batch, ext)
-        dump(batch_data, format, dumped_batch_file)
-
     return 0
+
+
+def run(red_file, fmt, prefix, **_):
+    secret_values = None
+    result = {
+        'state': 'succeeded',
+        'debugInfo': None
+    }
+    try:
+        ext = file_extension(fmt)
+
+        red_data = load_and_read(red_file, 'REDFILE')
+        secret_values = get_secret_values(red_data)
+
+        red_validation(red_data, False)
+
+        if 'batches' not in red_data:
+            wrapped_print([
+                'ERROR: REDFILE does not contain batches.'
+            ], error=True)
+            return 1
+
+        for batch in range(len(red_data['batches'])):
+            batch_data = convert_batch_experiment(red_data, batch)
+            dumped_batch_file = '{}batch_{}.{}'.format(prefix, batch, ext)
+            dump(batch_data, fmt, dumped_batch_file)
+    except Exception as e:
+        print_exception(e, secret_values)
+        result['debugInfo'] = exception_format(secret_values)
+        result['state'] = 'failed'
+
+    return result
